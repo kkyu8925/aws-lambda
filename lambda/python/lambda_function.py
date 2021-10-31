898771036ecc960d225720200ffd8af7587a320b
gusrb8925 <gusrb8925@naver.com>
2021-10-31 18:49:43 +0900

    21-10-31

--- lambda/python/lambda_function.py
+++ lambda/python/lambda_function.py
@@ -1,9 +1,11 @@
 import json
 import boto3
+import base64
 import pymysql
+from requests_toolbelt.multipart import decoder
 
 
-def get_secret():
+def create_connection_token():
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
@@ -17,7 +19,7 @@ def get_secret():
 
 
 def db_ops():
-    secrets = get_secret()
+    secrets = create_connection_token()
     try:
         connection = pymysql.connect(
             host=secrets['host'],
@@ -36,13 +38,37 @@ def db_ops():
     return connection
 
 
-def lambda_handler(event, context):
-    body = json.loads(event['body'])
+def uploadToS3(body, originalFileName):
+    s3 = boto3.client('s3')
+    s3.put_object(
+        ACL="public-read",
+        Bucket='kkyu-sparta',
+        Body=body,
+        Key=originalFileName,
+        ContentType='image/' + originalFileName.split('.')[1]
+    )
+
     conn = db_ops()
     cursor = conn.cursor()
-    cursor.execute("insert into board(title, content) value('" + body['title'] + "', '" + body['content'] + "')")
+    cursor.execute("insert into image(url) value('" + originalFileName + "')")
     conn.commit()
 
+
+def lambda_handler(event, context):
+    if 'Content-Type' in event['headers']:
+        content_type_header = event['headers']['Content-Type']
+    else:
+        content_type_header = event['headers']['content-type']
+
+    postdata = base64.b64decode(event['body']).decode('iso-8859-1')
+
+    lst = []
+    for part in decoder.MultipartDecoder(postdata.encode('utf-8'), content_type_header).parts:
+        lst.append(part.text)
+
+    decoder_file = decoder.MultipartDecoder(postdata.encode('utf-8'), content_type_header)
+    file_name = lst[1]  # 파일명은 한글이 아니어야 한다.
+    uploadToS3(lst[0].encode('iso-8859-1'), file_name)
     return {
         "statusCode": 200,
         "body": json.dumps({
