30ff85ca8ba917576b1b11abbf70e3b773164de1
kkyu <gusrb8925@naver.com>
2021-11-01 15:47:08 +0900

    21-11-01

--- lambda/board/lambda_function.py
+++ lambda/board/lambda_function.py
@@ -19,6 +19,7 @@ def get_secret():
 
 def db_ops():
     secrets = get_secret()
+
     try:
         connection = pymysql.connect(
             host=secrets['host'],
@@ -38,39 +39,80 @@ def db_ops():
 
 
 def lambda_handler(event, context):
-    type = event['queryStringParameters']['type']
+    conn = db_ops()
+    cursor = conn.cursor()
 
-    if type == 'write':
-        today = date.today()
-        body = json.loads(event['body'])
-        conn = db_ops()
-        cursor = conn.cursor()
-        cursor.execute("insert into bbs(title, content, regDate) value('" + body['title'] + "', '" + body[
-            'content'] + "', '" + today.strftime("%Y%m%d") + "')")
-        conn.commit()
-        body = json.dumps({
-            "message": "success",
-        })
-    elif type == 'list':
-        conn = db_ops()
-        cursor = conn.cursor()
-        cursor.execute("select * from bbs")
-        result = cursor.fetchall()
-        body = json.dumps({
-            "message": "success",
-            "data": json.dumps(result)
-        })
-    elif type == 'delete':
-        idx = event['queryStringParameters']['idx']
-        conn = db_ops()
-        cursor = conn.cursor()
-        cursor.execute("delete from bbs where idx=" + idx)
-        conn.commit()
-        body = json.dumps({
-            "message": "success",
-        })
+    try:
+        type = event['queryStringParameters']['type']
 
-    return {
-        "statusCode": 200,
-        "body": body,
-    }
+        if type == 'write':
+            if event['httpMethod'] == 'OPTIONS':
+                body = json.dumps({
+                    "message": "success",
+                })
+            else:
+                today = date.today()
+                body = json.loads(event['body'])
+
+                cursor.execute("insert into bbs(title, content, regDate) value('" + body['title'] + "', '" + body[
+                    'content'] + "', '" + today.strftime("%Y%m%d") + "')")
+                conn.commit()
+                body = json.dumps({
+                    "message": "success",
+                })
+        elif type == 'list':
+            paramWord = event['queryStringParameters']['word']
+
+            if not paramWord:
+                cursor.execute("select idx, title, regDate from bbs")
+                result = cursor.fetchall()
+            else:
+                cursor.execute("select idx, title, regDate from bbs where title like '%" + paramWord + "%'")
+                result = cursor.fetchall()
+
+            body = json.dumps({
+                "result": "success",
+                "data": result
+            })
+        elif type == 'read':
+            idx = event['queryStringParameters']['idx']
+
+            cursor.execute("select * from bbs where idx=" + idx)
+            bbs = cursor.fetchone()
+
+            body = json.dumps({
+                "result": "success",
+                "data": bbs
+            })
+        elif type == 'delete':
+            idx = event['queryStringParameters']['idx']
+
+            cursor.execute("delete from bbs where idx=" + idx)
+            conn.commit()
+
+            body = json.dumps({
+                "message": "success",
+            })
+        return {
+            "statusCode": 200,
+            # Cross Origin처리
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            "body": body,
+        }
+    except:
+        return {
+            "statusCode": 500,
+            # Cross Origin처리
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST,GET'
+            },
+            "body": json.dumps({
+                "message": "fail",
+            }),
+        }
