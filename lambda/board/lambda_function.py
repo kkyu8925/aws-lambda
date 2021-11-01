4a0a0bca1f6e43bc530bacceb05dbd589a153e4e
kkyu <gusrb8925@naver.com>
2021-11-01 16:00:08 +0900

    21-11-01

--- lambda/board/lambda_function.py
+++ lambda/board/lambda_function.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import pymysql
 from datetime import date
+import math
 
 
 def get_secret():
@@ -19,7 +20,6 @@ def get_secret():
 
 def db_ops():
     secrets = get_secret()
-
     try:
         connection = pymysql.connect(
             host=secrets['host'],
@@ -39,13 +39,12 @@ def db_ops():
 
 
 def lambda_handler(event, context):
+    paramType = event['queryStringParameters']['type']
     conn = db_ops()
     cursor = conn.cursor()
 
     try:
-        type = event['queryStringParameters']['type']
-
-        if type == 'write':
+        if paramType == 'write':
             if event['httpMethod'] == 'OPTIONS':
                 body = json.dumps({
                     "message": "success",
@@ -53,64 +52,93 @@ def lambda_handler(event, context):
             else:
                 today = date.today()
                 body = json.loads(event['body'])
-
                 cursor.execute("insert into bbs(title, content, regDate) value('" + body['title'] + "', '" + body[
                     'content'] + "', '" + today.strftime("%Y%m%d") + "')")
                 conn.commit()
                 body = json.dumps({
                     "message": "success",
                 })
-        elif type == 'list':
+        elif paramType == 'list':
             paramWord = event['queryStringParameters']['word']
+            paramCurrPage = event['queryStringParameters']['page']
+            paramPerPage = event['queryStringParameters']['perPage']
+
+            if paramCurrPage is None:
+                paramCurrPage = 1
+
+            if paramPerPage is None:
+                paramPerPage = 10
+
+            startPage = str((int(paramCurrPage) - 1) * int(paramPerPage))
 
             if not paramWord:
-                cursor.execute("select idx, title, regDate from bbs")
+                cursor.execute("select count(idx) as count from bbs")
+                count = cursor.fetchone()
+                totalCount = int(count['count'])
+                totalPage = math.ceil(totalCount / int(paramPerPage))
+                cursor.execute("select idx, title, regDate from bbs limit " + startPage + "," + paramPerPage)
                 result = cursor.fetchall()
             else:
-                cursor.execute("select idx, title, regDate from bbs where title like '%" + paramWord + "%'")
+                cursor.execute("select count(idx) as count from bbs  where title like '%" + paramWord + "%'")
+                count = cursor.fetchone()
+                totalCount = int(count['count'])
+                totalPage = math.ceil(totalCount / int(paramPerPage))
+                cursor.execute(
+                    "select idx, title, regDate from bbs where title like '%" + paramWord + "%' limit " + startPage + "," + paramPerPage)
+
                 result = cursor.fetchall()
 
             body = json.dumps({
                 "result": "success",
-                "data": result
+                "data": {
+                    "contents": result,
+                    "pageOptions": {"perPage": paramPerPage, "totalPage": totalPage, "currPage": paramCurrPage,
+                                    "totalCount": totalCount}
+                }
             })
-        elif type == 'read':
+        elif paramType == 'read':
             idx = event['queryStringParameters']['idx']
-
             cursor.execute("select * from bbs where idx=" + idx)
             bbs = cursor.fetchone()
-
             body = json.dumps({
                 "result": "success",
                 "data": bbs
             })
-        elif type == 'delete':
+        elif paramType == 'delete':
             idx = event['queryStringParameters']['idx']
-
             cursor.execute("delete from bbs where idx=" + idx)
             conn.commit()
-
             body = json.dumps({
                 "message": "success",
             })
+        elif paramType == 'deleteAll':
+            idxs = event['queryStringParameters']['idxs']
+            print(idxs)
+            cursor.execute("delete from bbs where idx in(" + idxs + ")")
+            conn.commit()
+            body = json.dumps({
+                "message": "success",
+            })
+
         return {
             "statusCode": 200,
             # Cross Origin처리
             'headers': {
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+                'Access-Control-Allow-Methods': 'POST,GET,DELETE'
             },
             "body": body,
         }
-    except:
+    except Exception as e:
+        print(e)
         return {
             "statusCode": 500,
             # Cross Origin처리
             'headers': {
                 'Access-Control-Allow-Headers': 'Content-Type',
                 'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'POST,GET'
+                'Access-Control-Allow-Methods': 'POST,GET,DELETE'
             },
             "body": json.dumps({
                 "message": "fail",
